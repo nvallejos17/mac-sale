18b3a14b11ffb55df1dbde397da9a04713d316ec
Ignacio Vallejos <nacho.vallejos17@outlook.com>
2021-04-01 22:09:09 -0300

    Add smaller in-the-box.png

--- src/components/Gallery/Gallery.component.js
+++ src/components/Gallery/Gallery.component.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'react-bootstrap/Image';
-import { GalleryContainer } from './Gallery.styled';
+import { GalleryWrapper, GalleryContainer } from './Gallery.styled';
 
 const Gallery = () => {
   const images = ['img/01-min.jpeg', 'img/02-min.jpeg'];
 
   return (
-    <GalleryContainer>
-      <Carousel fade>
-        {images.map((img) => (
-          <Carousel.Item key={img}>
-            <Image src={img} fluid />
-          </Carousel.Item>
-        ))}
-      </Carousel>
-    </GalleryContainer>
+    <GalleryWrapper>
+      <GalleryContainer>
+        <Carousel fade>
+          {images.map((img) => (
+            <Carousel.Item key={img}>
+              <Image src={img} fluid />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </GalleryContainer>
+    </GalleryWrapper>
   );
 };
 
--- src/components/Gallery/Gallery.styled.js
+++ src/components/Gallery/Gallery.styled.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import Container from 'react-bootstrap/Container';
 
+export const GalleryWrapper = styled.section`
+  margin: 2rem 0;
+`;
+
 export const GalleryContainer = styled(Container)`
   max-width: 768px;
 `;
