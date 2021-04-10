d931a829c60c079a86070da72c070a20ab0df4bc
Ignacio Vallejos <nacho.vallejos17@outlook.com>
2021-04-10 17:52:09 -0300

    Add Price.styled file

--- src/components/Price/Price.component.js
+++ src/components/Price/Price.component.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import {
+  PriceWrapper,
+  PriceItem,
+  PriceItemLabel,
+  PriceItemValue,
+} from './Price.styled';
 
 const Price = () => {
-  return <div>Hello, world!</div>;
+  const usd = 900;
+  const ars = usd * 145;
+  const dai = usd / 1.04;
+
+  const priceItems = [
+    { cur: 'usd', val: usd },
+    { cur: 'ars', val: ars },
+    { cur: 'dai', val: dai },
+  ];
+
+  return (
+    <PriceWrapper>
+      {priceItems.map(({ cur, val }, index) => (
+        <PriceItem key={index}>
+          <PriceItemLabel>{cur.toUpperCase()}</PriceItemLabel>
+          <PriceItemValue>{val.toFixed(0)}</PriceItemValue>
+        </PriceItem>
+      ))}
+    </PriceWrapper>
+  );
 };
 
 export default Price;
---
+++ src/components/Price/Price.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const PriceWrapper = styled.ul``;
+
+export const PriceItem = styled.li``;
+
+export const PriceItemLabel = styled.label``;
+
+export const PriceItemValue = styled.span``;
