4cce1d6abf07d958f1ade3a9b1166b5c55b21121
Ignacio Vallejos <nacho.vallejos17@outlook.com>
2021-04-11 03:10:42 -0300

    Uninstall axios. CORS handled

--- src/components/Price/Price.component.js
+++ src/components/Price/Price.component.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import {
   PriceWrapper,
   PriceItem,
@@ -12,12 +11,16 @@ const Price = ({ usd = 900 }) => {
   const [dai, setDai] = useState(null);
 
   useEffect(() => {
-    axios
-      .get('https://be.buenbit.com/api/market/tickers/')
-      .then((res) => {
+    fetch('https://be.buenbit.com/api/market/tickers/', {
+      mode: 'cors',
+      method: 'GET',
+      cache: 'no-cache',
+    })
+      .then((res) => res.json())
+      .then((json) => {
         const {
           object: { daiars, daiusd },
-        } = res.data;
+        } = json;
         setArs(usd * (daiars.selling_price / daiusd.purchase_price));
         setDai(usd / daiusd.purchase_price);
       })
