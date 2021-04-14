21db129ea88dd30b25e7f056c30641b66c9122d0
Ignacio Vallejos <nacho.vallejos17@outlook.com>
2021-04-13 21:46:41 -0300

    Remove unnecessary event listeners

--- src/components/SpecItem/SpecItem.component.js
+++ src/components/SpecItem/SpecItem.component.js
@@ -1,26 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { SpecItemContent, SpecItemRow, SpecItemTitle } from './SpecItem.styled';
 
 const SpecItem = ({ title, children }) => {
-  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
-  const [hasWrapped, setHasWrapped] = useState(innerWidth < 576);
-
-  useEffect(() => {
-    const onResize = () => setInnerWidth(window.innerWidth);
-    window.addEventListener('resize', onResize);
-    return () => window.removeEventListener('resize', onResize);
-  }, []);
-
-  useEffect(() => setHasWrapped(innerWidth < 576), [innerWidth]);
-
   return (
-    <SpecItemRow noGutters hasWrapped={hasWrapped}>
-      <SpecItemTitle md={4} hasWrapped={hasWrapped}>
+    <SpecItemRow noGutters>
+      <SpecItemTitle md={4}>
         <p>
           <strong>{title}</strong>
         </p>
       </SpecItemTitle>
-      <SpecItemContent hasWrapped={hasWrapped}>{children}</SpecItemContent>
+      <SpecItemContent>{children}</SpecItemContent>
     </SpecItemRow>
   );
 };
--- src/components/SpecItem/SpecItem.styled.js
+++ src/components/SpecItem/SpecItem.styled.js
@@ -1,83 +1,84 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { macGold } from '../../theme/colors';
 
-export const SpecItemRow = styled(Row)(
-  ({ hasWrapped }) => css`
-    * {
-      margin: 0;
-      padding: 0;
-    }
+export const SpecItemRow = styled(Row)`
+  * {
+    margin: 0;
+    padding: 0;
+  }
 
-    padding: ${hasWrapped ? 0 : '2rem 0'};
+  padding: 2rem 0;
+  line-height: 2;
+  border-bottom: 1px solid ${macGold};
 
-    line-height: 2;
+  &:first-of-type {
+    padding-top: 0;
 
-    border-bottom: ${hasWrapped ? 'none' : `1px solid ${macGold}`};
-
-    &:first-of-type {
+    > :first-child {
       padding-top: 0;
-
-      ${SpecItemTitle} {
-        padding-top: 0;
-      }
     }
+  }
 
-    &:last-child {
-      padding-bottom: 0;
-      border-bottom: 0;
-    }
-  `
-);
+  &:last-child {
+    padding-bottom: 0;
+    border-bottom: 0;
+  }
 
-export const SpecItemContent = styled(Col)(
-  ({ hasWrapped }) => css`
-    padding: ${hasWrapped ? '1rem 0' : 0};
+  @media (max-width: 576px) {
+    padding: 0;
+    border-bottom: none;
+  }
+`;
 
-    color: lightgrey;
+export const SpecItemContent = styled(Col)`
+  color: lightgrey;
 
-    strong,
-    b {
-      color: white;
-    }
-
-    a {
-      color: ${macGold};
-    }
-
-    ul {
-      list-style-type: disc;
-      list-style-position: inside;
-    }
-
-    > ul {
-      list-style-type: none;
-    }
-
-    > :last-child {
-      margin: 0;
-    }
-
-    > img {
-      max-width: 100%;
-      max-height: 200px;
-
-      display: block;
-    }
-  `
-);
-
-export const SpecItemTitle = styled(Col)(
-  ({ hasWrapped }) => css`
-    padding: ${hasWrapped ? '1rem' : 0};
-
+  strong,
+  b {
     color: white;
+  }
 
-    border-bottom: ${hasWrapped ? `1px solid ${macGold}` : 'none'};
+  a {
+    color: ${macGold};
+  }
+
+  ul {
+    list-style-type: disc;
+    list-style-position: inside;
+  }
+
+  > ul {
+    list-style-type: none;
+  }
+
+  > :last-child {
+    margin: 0;
+  }
+
+  > img {
+    max-width: 100%;
+    max-height: 200px;
+    display: block;
+  }
+
+  @media (max-width: 576px) {
+    padding: 1rem 0;
+  }
+`;
+
+export const SpecItemTitle = styled(Col)`
+  > * {
+    padding-right: 2rem;
+  }
+
+  @media (max-width: 576px) {
+    padding: 1rem;
+    border-bottom: 1px solid ${macGold};
 
     > * {
-      padding-right: ${hasWrapped ? 0 : '2rem'};
+      padding-right: 0;
     }
-  `
-);
+  }
+`;
