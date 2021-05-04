1f6e7bcba6829ac65921c37c2bded965803749f6
Ignacio Vallejos <nacho.vallejos17@outlook.com>
2021-05-03 21:07:07 -0300

    Add uuid. Implement uuid on Faqitem

--- src/components/Faq/Faq.component.js
+++ src/components/Faq/Faq.component.js
@@ -6,17 +6,14 @@ import FaqItem from '../FaqItem/FaqItem.component';
 const Faq = () => {
   const faqItems = [
     {
-      eventKey: '1',
       question: 'Question 1',
       answer: 'Answer 1',
     },
     {
-      eventKey: '2',
       question: 'Question 2',
       answer: 'Answer 2',
     },
     {
-      eventKey: '3',
       question: 'Question 3',
       answer: 'Answer 3',
     },
@@ -25,8 +22,8 @@ const Faq = () => {
   return (
     <Section title='Preguntas frecuentes'>
       <Accordion>
-        {faqItems.map((faqItem) => (
-          <FaqItem key={faqItem.eventKey} faqItem={faqItem} />
+        {faqItems.map((faqItem, index) => (
+          <FaqItem key={index} faqItem={faqItem} />
         ))}
       </Accordion>
     </Section>
--- src/components/FaqItem/FaqItem.component.js
+++ src/components/FaqItem/FaqItem.component.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { v4 as uuid } from 'uuid';
 import Card from 'react-bootstrap/Card';
 import Accordion from 'react-bootstrap/Accordion';
 
 const FaqItem = ({ faqItem }) => {
-  const { eventKey, question, answer } = faqItem;
+  const { question, answer } = faqItem;
+  const eventKey = uuid();
 
   return (
     <Card>
-      <Accordion.Toggle as={Card.Header} eventKey={eventKey}>
+      <Accordion.Toggle
+        as={Card.Header}
+        eventKey={eventKey}
+        style={{ cursor: 'pointer' }}
+      >
         {question}
       </Accordion.Toggle>
       <Accordion.Collapse eventKey={eventKey}>
