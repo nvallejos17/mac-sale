22dde366329dcf88f7f933eac1496303d91c1527
Ignacio Vallejos <nacho.vallejos17@outlook.com>
2021-05-22 11:57:50 -0300

    Remove Price from MainNav

--- src/components/MainNav/MainNav.component.js
+++ src/components/MainNav/MainNav.component.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import Price from '../Price/Price.component';
 
 const MainNav = () => {
   return (
     <Navbar bg='dark' variant='dark' sticky='top' expand={false}>
       <Navbar.Brand>MacBook Air</Navbar.Brand>
-      <Price />
     </Navbar>
   );
 };
